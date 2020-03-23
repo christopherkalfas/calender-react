560fd85a7201a4a99e4fed539db03a6c290a5786
Chris Kalfas <christopherkalfas22@gmail.com>
2020-03-23 16:10:10 -0400

    Was able to console log all the days in the month from moment and added class names to each instance of day coming back.

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -4,18 +4,52 @@ import moment from "moment"
 
 export default class Calender extends React.Component {
 
+    state = {
+        dateObject: moment()
+    }
     weekdayShort = moment.weekdaysShort()
+
+    firstDayOfMonth = () => {
+        let dateObject = this.state.dateObject
+        let firstDay = moment(dateObject).startOf('month').format('d')
+        return firstDay
+    }
+
+    daysInMonth = () => {
+        let dayObject = this.state.dateObject.daysInMonth()
+        return dayObject
+    }
 
     
     render(){
-        let weekdaySname = this.weekdayShort.map(day => {
+        let weekday = this.weekdayShort.map(day => {
             return(
                 <th key={day} className="weekday">
                     {day}
                 </th>
     
             )
-        });
+        })
+
+        let blankDays = []
+        for( let i = 0; i < this.firstDayOfMonth(); i += 1){
+            blankDays.push(
+                <td key={i} className="calender-day blank">
+                    {""}
+                </td>)
+        }
+        console.log("Blank Spaces: ", blankDays)
+
+        let daysInMonth = []
+        for( let day = 1; day <= this.daysInMonth(); day += 1){
+            daysInMonth.push(
+                <td key={day} className="calender-day">
+                    {day}
+                </td>
+            )
+        }
+
+        console.log("Day: ", daysInMonth)
 
 
         return(
@@ -24,7 +58,7 @@ export default class Calender extends React.Component {
                 <table className="calender">
                     <tbody>
                         <tr>
-                            {weekdaySname}
+                            {weekday}
                         </tr>
                     </tbody>       
                 </table>
