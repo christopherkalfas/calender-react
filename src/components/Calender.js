3a7a447addf5274dc7920b1fb71dda9e40410166
Chris Kalfas <christopherkalfas22@gmail.com>
2020-03-23 15:21:48 -0400

    Successfully rendered data from momnent package. Laid out table and add a little css.

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -1,10 +1,33 @@
 import React from 'react'
 
+import moment from "moment"
+
 export default class Calender extends React.Component {
+
+    weekdayShort = moment.weekdaysShort()
+
+    
     render(){
+        let weekdaySname = this.weekdayShort.map(day => {
+            return(
+                <th key={day} className="weekday">
+                    {day}
+                </th>
+    
+            )
+        });
+
+
         return(
             <div className="calender-container">
                 <h1>Kalfender</h1>
+                <table className="calender">
+                    <tbody>
+                        <tr>
+                            {weekdaySname}
+                        </tr>
+                    </tbody>       
+                </table>
             </div>
         )
     }
