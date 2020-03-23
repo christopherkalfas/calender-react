856c09c02e1b0f06e60c16a54154daf7fdd3591a
Chris Kalfas <christopherkalfas22@gmail.com>
2020-03-23 14:54:39 -0400

    installed moment for date formater and mapped out init components.

---
+++ src/components/Calender.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Calender extends React.Component {
+    render(){
+        return(
+            <div className="calender-container">
+                <h1>Kalfender</h1>
+            </div>
+        )
+    }
+}
