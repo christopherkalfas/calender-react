d02f34a0a5d0b965f7f92a98aac3bdd26b47553b
Chris Kalfas <christopherkalfas22@gmail.com>
2020-03-30 14:36:14 -0400

    I am using a less involved pack to help with styling call fontawesome. I installed the package  via CLI and was able to render a icon as a component to test correct installation.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 import Calendar from "./components/Calendar"
 import './App.css';
+
 
 
 function App() {
   return (
     <div className="App">
       <Calendar />
+      <FontAwesomeIcon icon={faCoffee} />
     </div>
   );
 }
